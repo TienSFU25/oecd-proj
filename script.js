db02996c64a10c5f79268e6a0272357a353bb8df
Tien Vu <tienv2510@gmail.com>
2019-11-14 19:51:03 -0800

    Add 2x2 layout

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ const noop = (e) => {d3.event.stopPropagation()};
 const geographyDataLoc = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson";
 const worldTopologyDataLoc = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv";
 
-const margin = {top: 40, right: 20, bottom: 30, left: 40},
+const margin = {top: 10, right: 0, bottom: 0, left: 10},
     width = window.innerWidth - margin.left - margin.right,
     height = window.innerHeight - margin.top - margin.bottom;
 
+const actualWidth = width - margin.left - margin.right;
+const actualHeight = height - margin.top - margin.bottom;
+
+let featureBounds;
+let geoLayer = {};
+let singleViewWidth = actualWidth / 2;
+let singleViewHeight = actualHeight / 2;
+
 // The svg
 var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-// Map and projection
-var projection = d3.geoMercator()
-    .scale(70)
-    .center([0,20])
-    .translate([width / 2, height / 2]);
+    .attr("width", actualWidth)
+    .attr("height", actualHeight);
 
 // just use a default projection
 // projection = d3.geoEqualEarth();
-projection = d3.geoNaturalEarth1();
+var projection = d3.geoNaturalEarth1();
 
 // Data and color scale
 var data = d3.map();
@@ -33,13 +35,38 @@ const dataPromises = [
 ];
 
 Promise.all(dataPromises).then(values => {
-    values[1].map((value, index) => {
+    values[1].map((value) => {
         data.set(value.code, +value.pop);
     });
     ready(values[0]);
 }).catch(error => console.error(`Error in data fetching ${error}`));
 
-var theMap = svg.append("g");
+var layout = svg
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+let quads = [];
+
+// make our layout
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+        let quad = layout.append("g");
+
+        // just to make a checkered layout
+        let opacity = (i + j) % 2 == 0 ? 0.3 : 0.1;
+
+        quad.append("rect")
+            .attr("opacity", opacity)
+            .attr("width", singleViewWidth)
+            .attr("height", singleViewHeight)
+            .attr("transform", "translate(" + singleViewWidth*i + "," + singleViewHeight*j + ")");
+
+        quads.push(quad);
+    }
+}
+
+var theMap = quads[0]
+    .append("g");
 
 // Define the div for the tooltip
 const tooltip = d3.select("body").append("div")	
@@ -62,19 +89,50 @@ const updateTooltip = (polygon) => {
         .style("top", (d3.event.pageY) + "px");	
 };
 
+function getFeaturesBox() {
+    return {
+        x: featureBounds[0][0],
+        y: featureBounds[0][1],
+        width: featureBounds[1][0] - featureBounds[0][0],
+        height: featureBounds[1][1] - featureBounds[0][1]
+    };
+};
+
+// fits the geometry layer inside the viewport
+function fitGeoInside() {
+    var bbox = getFeaturesBox(),
+        scale = 0.95 / Math.max(bbox.width / singleViewWidth, bbox.height / singleViewHeight),
+        trans = [-(bbox.x + bbox.width / 2) * scale + singleViewWidth / 2, -(bbox.y + bbox.height / 2) * scale + singleViewHeight / 2];
+
+    geoLayer.scale = scale;
+    geoLayer.translate = trans;
+
+    theMap.attr('transform', [
+            'translate(' + geoLayer.translate + ')',
+            'scale(' + geoLayer.scale + ')'
+        ].join(' '));
+}
+
 function ready(worldTopology) {
     // Draw the map
     let mapGroup = theMap
         .selectAll("path")
         .data(worldTopology.features)
         .enter()
-        .append("g");  
+        .append("g");
+
+    const path = d3.geoPath().projection(projection);
+
+    var collection = {
+        'type': 'FeatureCollection',
+        'features' : worldTopology.features
+      };
+
+    featureBounds = path.bounds(collection);
         
     mapGroup.append("path")
         // draw each country
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
+        .attr("d", path)
         // set the color of each country
         .attr("fill", function (d) {
             d.total = data.get(d.id) || 0;
@@ -122,4 +180,6 @@ function ready(worldTopology) {
             .duration(500)		
             .style("opacity", 0);
     });
+
+    fitGeoInside();
 }
