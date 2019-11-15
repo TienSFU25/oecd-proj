47f6f748791eeddfb20a2913b734f1bdd564acdd
Tien Vu <tienv2510@gmail.com>
2019-11-14 20:39:53 -0800

    Add boxplot

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const noop = (e) => {d3.event.stopPropagation()};
 const geographyDataLoc = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson";
 const worldTopologyDataLoc = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv";
+const irisDataLoc = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv";
 
 const margin = {top: 10, right: 0, bottom: 0, left: 10},
     width = window.innerWidth - margin.left - margin.right,
@@ -31,14 +32,16 @@ var colorScale = d3.scaleThreshold()
 
 const dataPromises = [
     d3.json(geographyDataLoc),
-    d3.csv(worldTopologyDataLoc)
+    d3.csv(worldTopologyDataLoc),
+    d3.csv(irisDataLoc)
 ];
 
 Promise.all(dataPromises).then(values => {
     values[1].map((value) => {
         data.set(value.code, +value.pop);
     });
-    ready(values[0]);
+    drawWorldView(values[0]);
+    drawBoxPlot(values[2]);
 }).catch(error => console.error(`Error in data fetching ${error}`));
 
 var layout = svg
@@ -50,7 +53,8 @@ let quads = [];
 // make our layout
 for (let i = 0; i < 2; i++) {
     for (let j = 0; j < 2; j++) {
-        let quad = layout.append("g");
+        let quad = layout.append("g")
+            .attr("transform", "translate(" + singleViewWidth*i + "," + singleViewHeight*j + ")");
 
         // just to make a checkered layout
         let opacity = (i + j) % 2 == 0 ? 0.3 : 0.1;
@@ -58,8 +62,7 @@ for (let i = 0; i < 2; i++) {
         quad.append("rect")
             .attr("opacity", opacity)
             .attr("width", singleViewWidth)
-            .attr("height", singleViewHeight)
-            .attr("transform", "translate(" + singleViewWidth*i + "," + singleViewHeight*j + ")");
+            .attr("height", singleViewHeight);
 
         quads.push(quad);
     }
@@ -113,7 +116,7 @@ function fitGeoInside() {
         ].join(' '));
 }
 
-function ready(worldTopology) {
+function drawWorldView(worldTopology) {
     // Draw the map
     let mapGroup = theMap
         .selectAll("path")
@@ -182,4 +185,88 @@ function ready(worldTopology) {
     });
 
     fitGeoInside();
+}
+
+function drawBoxPlot(data) {
+    // pretty random constants...not exactly responsive
+    // we will need to tweak this to fit the screen
+    const boxScale = 0.9;
+    const boxLeftShift = singleViewWidth * (1 - boxScale) / 2;
+    const boxDownShift = singleViewHeight * (1 - boxScale) / 3;
+
+    // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
+    var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+        .key(function(d) { return d.Species;})
+        .rollup(function(d) {
+            q1 = d3.quantile(d.map(function(g) { return g.Sepal_Length;}).sort(d3.ascending),.25)
+            median = d3.quantile(d.map(function(g) { return g.Sepal_Length;}).sort(d3.ascending),.5)
+            q3 = d3.quantile(d.map(function(g) { return g.Sepal_Length;}).sort(d3.ascending),.75)
+            interQuantileRange = q3 - q1
+            min = q1 - 1.5 * interQuantileRange
+            max = q3 + 1.5 * interQuantileRange
+            
+            return({q1: q1, median: median, q3: q3, interQuantileRange: interQuantileRange, min: min, max: max})
+        })
+        .entries(data);
+
+    // Show the X scale
+    var x = d3.scaleBand()
+        .range([ 0, singleViewWidth ])
+        .domain(["setosa", "versicolor", "virginica"])
+        .paddingInner(1)
+        .paddingOuter(.5);
+
+    var boxplot = quads[1].append("g")
+        .attr("transform", `scale(${boxScale}) translate(${boxLeftShift}, ${boxDownShift})`);
+
+    boxplot.append("g")
+        .attr("transform", `translate(0, ${singleViewHeight})`)
+        .call(d3.axisBottom(x));
+
+    // Show the Y scale
+    var y = d3.scaleLinear()
+        .domain([3,9])
+        .range([singleViewHeight, 0])
+    boxplot.append("g")
+        .call(d3.axisLeft(y));
+
+    // Show the main vertical line
+    boxplot
+        .selectAll("vertLines")
+        .data(sumstat)
+        .enter()
+        .append("line")
+        .attr("x1", function(d){return(x(d.key))})
+        .attr("x2", function(d){return(x(d.key))})
+        .attr("y1", function(d){return(y(d.value.min))})
+        .attr("y2", function(d){return(y(d.value.max))})
+        .attr("stroke", "black")
+        .style("width", 40);
+
+    // rectangle for the main box
+    var boxWidth = 100
+    boxplot
+        .selectAll("boxes")
+        .data(sumstat)
+        .enter()
+        .append("rect")
+            .attr("x", function(d){return(x(d.key)-boxWidth/2)})
+            .attr("y", function(d){return(y(d.value.q3))})
+            .attr("height", function(d){return(y(d.value.q1)-y(d.value.q3))})
+            .attr("width", boxWidth )
+            .attr("stroke", "black")
+            .style("fill", "#69b3a2");
+
+    // Show the median
+    boxplot
+        .selectAll("medianLines")
+        .data(sumstat)
+        .enter()
+        .append("line")
+        .attr("x1", function(d){return(x(d.key)-boxWidth/2) })
+        .attr("x2", function(d){return(x(d.key)+boxWidth/2) })
+        .attr("y1", function(d){return(y(d.value.median))})
+        .attr("y2", function(d){return(y(d.value.median))})
+        .attr("stroke", "black")
+        .style("width", 80);
 }
