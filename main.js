85e964745da60c8a49f9246b21c2c17a56c68950
vivek <vvyogi@gmail.com>
2019-11-16 00:09:03 -0800

    Added bars to the line plot

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ window.onload = function(){
 
     //console.log("Create a global variable named \"_data\"");
     //loadMapData("world-110m2.json");
-    loadSkillData('SKILLS_2018_TOTAL_25102019044734209.csv');
+    //loadSkillData('SKILLS_2018_TOTAL_25102019044734209.csv');
+    loadSkillData('https://raw.githubusercontent.com/cusoh/data_skills/master/SKILLS_2018_TOTAL_25102019044734209.csv');
     createDropDown("#float_bottom");
 };
 
@@ -76,7 +77,7 @@ function updateBarPlot(country, data){
     console.log(myheight);
 
 
-    var margin = {top: 0, right: 0, bottom: 200, left: 10},
+    var margin = {top: 0, right: 0, bottom: 200, left: 0},
         width = 2200 - margin.left - margin.right,
         height = myheight - margin.top - margin.bottom;
 
@@ -93,7 +94,7 @@ function updateBarPlot(country, data){
             "translate(" + margin.left + "," + margin.top + ")");
     // Add x axis
     var x = d3.scaleBand()
-        .domain(data.map( function(d,i) {
+        .domain( data.map( function(d,i) {
             return d.key; }))
         .rangeRound([0, width])
         .padding(0.1);
@@ -102,8 +103,8 @@ function updateBarPlot(country, data){
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
-        .attr("y", 20)
-        .attr("x", 9)
+        .attr("y", 0)
+        .attr("x", 10)
         .attr("dy", ".5em")
         .attr("transform", "rotate(90)")
         .style("text-anchor", "start");
@@ -119,15 +120,15 @@ function updateBarPlot(country, data){
         .call(d3.axisLeft(y));
 
     // Add the line
-    svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-            .x(function(d,i) { return x(d.key) })
-            .y(function(d,i) { return y(d.value) })
-        );
+    // svg.append("path")
+    //     .datum(data)
+    //     .attr("fill", "none")
+    //     .attr("stroke", "steelblue")
+    //     .attr("stroke-width", 1.5)
+    //     .attr("d", d3.line()
+    //         .x(function(d,i) { return x(d.key) })
+    //         .y(function(d,i) { return y(d.value) })
+    //     );
 
     // Add the line
     svg.append("path")
@@ -163,7 +164,7 @@ function updateBarPlot(country, data){
         .attr("fill", function(d,i){
             //let v = parseFloat(d.value);
             let v = 0.55;
-            console.log(d);
+            //console.log(d);
             if(v>=0.5){return '#FBB';}else{return '#BFB';}
         })
         .attr("stroke", "none")
@@ -173,6 +174,70 @@ function updateBarPlot(country, data){
             .y0(function(d) { return y(0) })
             .y1(function(d) { if (d.value<0) {return y(d.value)}else{return y(0)} })
         );
+
+    svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.key); } )
+        .attr("cy", function (d) { return y(d.value); } )
+        .attr("r", 2.0)
+        .style("fill", "orange");
+
+    svg.selectAll("rect").data(data)
+        .enter()
+        .append("rect")
+        .attr("x",function(d,i){
+            //d is the object
+            //i is the index
+            console.log(i);
+            return x(d.key)-(0.5*width/data.length); })
+        .attr("y", function(d,i){
+            let u = y(0); let v = y(d.value);
+            if(u>v)
+                return v;
+            else
+                return u;
+
+        })
+        .attr("width", -2 + width/data.length)
+        .attr("height", function(d,i){
+            let u = y(0); let v = y(d.value);
+            if(u>v)
+                return u-v;
+            else
+                return v-u;
+        })
+        .attr("fill",'white')
+        .attr('opacity', '0.3')
+        .on("mouseover", function(d,i){
+            //console.log("Mouse over");
+            //console.log(d['Country']);
+            let thise = d3.select(this);
+            thise.transition()
+                .duration('50')
+                .attr('opacity', '.8')
+                .attr("transform", function(){
+                    let center = {
+                        x : thise.attr("x"),
+                        y : thise.attr("y")
+                    };
+                    return `translate(${thise.attr("transform_origin_x")}, ${thise.attr("transform_origin_y")}) 
+                    translate(${-0.2 * center.x}, ${-0.2 * center.y})
+                    scale(1.2, 1.2)`;
+                });
+        })
+        .on('mouseout', function(d,i){
+            //console.log("Bye from ");
+            let thise=d3.select(this);
+            thise.transition()
+                .duration('50')
+                .attr('opacity', '0.3')
+                .attr("transform", function(){
+                    return `translate(${thise.attr("transform_origin_x")}, ${thise.attr("transform_origin_y")}) scale(1,1)`;
+                });
+        });
 
     // // Add the line
     // svg.append("path")
